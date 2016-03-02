51fa7558878ae821072674a51334fe5b6eda7d7d
Tryan18 <Tryan18@gmail.com>
2016-03-02 14:14:53 +0100

    BundleExample
    Shows an example how to transfer data from one Activity to another! :)

---
+++ BundleExample/BundleExample/Activity2.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace BundleExample
+{
+	[Activity (Label = "Activity2")]			
+	public class Activity2 : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Create your application here
+			if(bundle != null)
+			{
+				
+				string s = bundle.GetString ("key");
+			}
+			string ss = Intent.GetStringExtra ("key");
+		}
+	}
+}
+
---
+++ BundleExample/BundleExample/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace BundleExample
+{
+	[Activity (Label = "BundleExample", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+			
+			button.Click += delegate {
+				Intent activity2Intent = new Intent(this,typeof(Activity2));
+				Bundle b = new Bundle();
+				b.PutString("key"," test");
+				activity2Intent.PutExtras(b);
+				StartActivity(activity2Intent);
+			};
+		}
+	}
+}
+
+
