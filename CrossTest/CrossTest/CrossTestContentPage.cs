c1e9ccca2165607cd4695f0b22a8ec41e91cc863
Tryan18 <Tryan18@gmail.com>
2016-05-12 11:31:34 +0200

    CrossTest
    Example Application of Cross Platform App with xamarin forms

---
+++ CrossTest/CrossTest/CrossTestContentPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace CrossTest
+{
+	public class CrossTestContentPage : ContentPage
+	{
+		public CrossTestContentPage ()
+		{
+			Content = new StackLayout { 
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
+
---
+++ CrossTest/CrossTest/MyPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CrossTest
+{
+	public partial class MyPage : ContentPage
+	{
+		public MyPage ()
+		{
+			InitializeComponent ();
+
+			//Add your Code here!
+		}
+
+		void OnMainContentViewSizeChanged(object sender, EventArgs args)
+		{
+
+		}
+
+		void OnBoardContentViewSizeChanged(object sender, EventArgs args)
+		{
+
+		}
+
+		async void OnButtonClicked(object sender, EventArgs e)
+		{
+			await DisplayAlert ("Alert", "You have been alerted", "OK");
+		}
+	}
+}
+
